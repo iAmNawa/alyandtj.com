efd8fbe1bcdbadf85d4d70f1f14f96f80337f468
Paul Borawski <paulborawski14@gmail.com>
2016-08-07 02:34:16 -0700

    leading zeros

--- public/script.js
+++ public/script.js
@@ -1,11 +1,11 @@
 const deadline = 'November 11 2016 23:59:59 GMT+0200'
 
 function getTimeRemaining(endtime){
-  var t = Date.parse(endtime) - Date.parse(new Date());
-  var seconds = Math.floor( (t/1000) % 60 );
-  var minutes = Math.floor( (t/1000/60) % 60 );
-  var hours = Math.floor( (t/(1000*60*60)) % 24 );
-  var days = Math.floor( t/(1000*60*60*24) );
+  var t = Date.parse(endtime) - Date.parse(new Date())
+  var seconds = Math.floor( (t/1000) % 60 )
+  var minutes = Math.floor( (t/1000/60) % 60 )
+  var hours = Math.floor( (t/(1000*60*60)) % 24 )
+  var days = Math.floor( t/(1000*60*60*24) )
   return {
     'total': t,
     'days': days,
@@ -16,22 +16,22 @@ function getTimeRemaining(endtime){
 }
 
 function initializeClock(id, endtime){
-  var clock = document.getElementById(id);
-  var daysSpan = clock.querySelector('.days');
-  var hoursSpan = clock.querySelector('.hours');
-  var minutesSpan = clock.querySelector('.minutes');
-  var secondsSpan = clock.querySelector('.seconds');
+  var clock = document.getElementById(id)
+  var daysSpan = clock.querySelector('.days')
+  var hoursSpan = clock.querySelector('.hours')
+  var minutesSpan = clock.querySelector('.minutes')
+  var secondsSpan = clock.querySelector('.seconds')
   function updateClock(){
-    var t = getTimeRemaining(endtime);
+    var t = getTimeRemaining(endtime)
 
-    daysSpan.innerHTML = t.days;
-    hoursSpan.innerHTML = t.hours;
-    minutesSpan.innerHTML = t.minutes;
-    secondsSpan.innerHTML = t.seconds;
+    daysSpan.innerHTML = ('0' + t.days).slice(-2)
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2)
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2)
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2)
   }
 
   updateClock(); // run function once at first to avoid delay
-  var timeinterval = setInterval(updateClock,1000);
+  var timeinterval = setInterval(updateClock,1000)
 }
 
 initializeClock('clockdiv', deadline)
